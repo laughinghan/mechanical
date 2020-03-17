a6a28916919187bd52bd1d098879e3fa75f11fb5
Han Seoul-Oh <laughinghan@gmail.com>
2020-03-17 01:09:11 -0700

    Fix ExponentExpr, add MultExpr and AddExpr
    Solve the left-recursion problem (which otherwise infinite-loops in
    top-down parsing strategies like parser combinators) by manually
    re-implementing .sepBy()

--- mechc.ts
+++ mechc.ts
@@ -1,10 +1,29 @@
-import P from 'parsimmon'
+import {
+  Parser,
+  createLanguage,
+  alt,
+  seq,
+  seqMap,
+  seqObj,
+  string as s,
+  regex as r
+} from 'parsimmon'
 
-export const parser = P.createLanguage({
+// who says you can't do left-recursion in a top-down parser? Come at me!
+function leftRecur<Expr, Op>(
+  argParser: Parser<Expr>,
+  opParser: Parser<Op>,
+  map: (op: Op, left: Expr, right: Expr) => Expr
+): Parser<Expr> {
+  return seqMap(argParser, seq(opParser, argParser).many(), (first, rest) =>
+    rest.reduce((left, [op, right]) => map(op, left, right), first))
+}
+
+export const parser = createLanguage({
   // Whitespace
-  _: () => P.regex(/[ \t]*/),  // optional, non-newline whitespace
-  __: () => P.regex(/[ \t]+/), // required, non-newline whitespace
-  _n: () => P.regex(/[ \t\n]+/), // newline or whitespace---no other whitespace allowed,
+  _: () => r(/[ \t]*/),  // optional, non-newline whitespace
+  __: () => r(/[ \t]+/), // required, non-newline whitespace
+  _n: () => r(/[ \t\n]+/), // newline or whitespace---no other whitespace allowed,
                                  // \a\b\v\f\r are all banned.
                                  // Banned whitespace should be treated kinda like control
                                  // characters and non-printing characters
@@ -13,10 +32,10 @@ export const parser = P.createLanguage({
   // Expression Grammar (based on JS)
   //   https://tc39.es/ecma262/#sec-ecmascript-language-expressions
   //
-  Identifier: () => P.regex(/[a-z]\w*/i), // TODO non-English letters etc
-  Numeral: () => P.regex(/[+-]?\d+/), // TODO decimals, exponential notation (negation?)
+  Identifier: () => r(/[a-z]\w*/i), // TODO non-English letters etc
+  Numeral: () => r(/\d+/), // TODO decimals, exponential notation
 
-  PrimaryExpr: L => P.alt( // in terms of operator precedence,
+  PrimaryExpr: L => alt( // in terms of operator precedence,
       // "primary expressions" are the smallest units, which are either actual
       // leaf nodes (variables, numberals, string literals) or delimited groups
       // (parenthesized exprs, array literals, object literals, function calls,
@@ -26,36 +45,53 @@ export const parser = P.createLanguage({
     L.Numeral,
   ),
 
-  UnaryExpr: L => P.alt( // unary operators (tightest-binding operators)
-    P.seqMap(P.regex(/[-!]/), L._, L.PrimaryExpr,
-      (op, _, arg) => ({ type: 'Unary', op, arg })),
-    L.PrimaryExpr,
+  UnaryExpr: ({_, PrimaryExpr }) => alt( // unary operators (tightest-binding operators)
+    PrimaryExpr,
+    seqMap(r(/[-!]/), _, PrimaryExpr,
+      (op, _, arg) => ({ type: 'UnaryExpr', op, arg })),
   ),
 
-  ExponentiationExpr: L => P.alt(
-    P.seqMap(L.PrimaryExpr, L._, P.string('**'), L._, L.UnaryExpr,
-      (base, _, __, ___, exponent) => ({ type: 'Exponentiation', base, exponent })),
-        // note that as a special exception to typical operator precedence,
-        // exponentiation binds equally tightly leftwards as UnaryExpr, to avoid
-        // the ambiguity of whether -2**2 is (-2)**2 = 4 or -(2**2) = -4
+  ExponentExpr: ({ PrimaryExpr, _, UnaryExpr }) => alt(
+    seqMap(PrimaryExpr, _, s('**'), _, UnaryExpr,
+      (left, _, __, ___, right) => ({ type: 'BinaryExpr', op: '**', left, right })),
+        // Note 1: as a special exception to typical operator precedence,
+        // exponents binds equally tightly leftwards as UnaryExpr, to avoid the
+        // ambiguity of whether -2**2 is (-2)**2 = 4 or -(2**2) = -4
         //   https://tc39.es/ecma262/#sec-exp-operator
-    L.PrimaryExpr,
+        // Chrome throws:
+        //   Uncaught SyntaxError: Unary operator used immediately before
+        //   exponentiation expression. Parenthesis must be used to disambiguate
+        //   operator precedence
+        //
+        // Note 2: in a breaking change from JS, Mechanical also prohibits
+        // chaining of exponents because it's not associative, i.e. it's
+        // ambiguous whether 2**3**2 is (2**3)**2 = 64 or 2**(3**2) = 512.
+        // In JS it's right-associative (so the answer is 512), which I think
+        // is confusing because the other non-associative operators are
+        // left-associative, e.g. 2/3/4 = (2/3)/4 and not 2/(3/4)
+    UnaryExpr,
+  ),
+  MultExpr: ({ ExponentExpr, _ }) => leftRecur(
+    ExponentExpr, r(/[*/%]/).trim(_),
+    (op, left, right) => ({ type: 'BinaryExpr', op, left, right })
+  ),
+  AddExpr: ({ MultExpr, _ }) => leftRecur(
+    MultExpr, r(/[+-]/).trim(_),
+    (op, left, right) => ({ type: 'BinaryExpr', op, left, right })
   ),
 
   //
   // Top-Level Declarations
   //
   StateDecl: ({ _, __, Identifier }) =>
-    P.seqObj<{var_name: string}, 'var_name'>(
-      P.string('State'),
+    seqObj<{var_name: string}, 'var_name'>(
+      s('State'),
       __,
       ['var_name', Identifier],
       _,
-      P.string('='),
+      s('='),
       _,
     )
 
   // top-level statements in an event handler declaration
 })
-
-console.log(parser.StateDecl.tryParse('State x = '))
--- tests.ts
+++ tests.ts
@@ -10,13 +10,115 @@ suite('Parser', () => {
     assert.deepStrictEqual(observed, expected)
   })
 
-  test('ExponentiationExpr', () => {
-    const observed = parser.ExponentiationExpr.tryParse('-2**2')
-    const expected = {
-      type: 'Exponentiation',
-      base: '-2',
-      exponent: '2',
-    }
-    assert.deepStrictEqual(observed, expected)
+  suite('UnaryExpr', () => {
+    test('basic -2', () => {
+      const observed = parser.UnaryExpr.tryParse('-2')
+      const expected = {
+        type: 'UnaryExpr',
+        op: '-',
+        arg: '2',
+      }
+      assert.deepStrictEqual(observed, expected)
+    })
+    test('fallback to PrimaryExpr', () => {
+      const observed = parser.UnaryExpr.tryParse('2')
+      const expected = '2'
+      assert.deepStrictEqual(observed, expected)
+    })
+  })
+
+  suite('ExponentExpr', () => {
+    test('basic 2**3', () => {
+      const observed = parser.ExponentExpr.tryParse('2**3')
+      const expected = {
+        type: 'BinaryExpr',
+        op: '**',
+        left: '2',
+        right: '3',
+      }
+      assert.deepStrictEqual(observed, expected)
+    })
+    test('un-op exponent 2**-3', () => {
+      const observed = parser.ExponentExpr.tryParse('2**-3')
+      const expected = {
+        type: 'BinaryExpr',
+        op: '**',
+        left: '2',
+        right: {
+          type: 'UnaryExpr',
+          op: '-',
+          arg: '3',
+        },
+      }
+      assert.deepStrictEqual(observed, expected)
+    })
+    test('fallthru to UnaryExpr', () => {
+      const observed = parser.ExponentExpr.tryParse('-2')
+      const expected = {
+        type: 'UnaryExpr',
+        op: '-',
+        arg: '2',
+      }
+      assert.deepStrictEqual(observed, expected)
+    })
+    test('prohibit ambiguous -2**2', () => {
+      // see comment in ExponentExpr source code for more about this ambiguity
+      assert.throws(() => {
+        parser.ExponentExpr.tryParse('-2**2')
+      })
+    })
+  })
+
+  suite('arithmetic precedence', () => {
+    test('MultExpr is left-associative', () => {
+      const observed = parser.MultExpr.tryParse('2/3/4')
+      const expected = {
+        type: 'BinaryExpr',
+        op: '/',
+        left: {
+          type: 'BinaryExpr',
+          op: '/',
+          left: '2',
+          right: '3',
+        },
+        right: '4',
+      }
+      assert.deepStrictEqual(observed, expected)
+    })
+    test('the MDAS (in PEMDAS)', () => {
+      const observed = parser.AddExpr.tryParse('2-3*4+5**6/7**8')
+      const expected = {
+        type: 'BinaryExpr',
+        op: '+',
+        left: {
+          type: 'BinaryExpr',
+          op: '-',
+          left: '2',
+          right: {
+            type: 'BinaryExpr',
+            op: '*',
+            left: '3',
+            right: '4',
+          },
+        },
+        right: {
+          type: 'BinaryExpr',
+          op: '/',
+          left: {
+            type: 'BinaryExpr',
+            op: '**',
+            left: '5',
+            right: '6',
+          },
+          right: {
+            type: 'BinaryExpr',
+            op: '**',
+            left: '7',
+            right: '8',
+          },
+        },
+      }
+      assert.deepStrictEqual(observed, expected)
+    })
   })
 })
